d6ea20bdfc1fd6716487543848bf56f2dd76b65a
reyes <ca93046@gmail.com>
2023-09-11 16:52:19 -0600

    se codifico el login

--- php/login.php
+++ php/login.php
@@ -2,19 +2,26 @@
 <html>
 <head>
   <style>
-    table {
-      border-collapse: collapse;
-      width: 100%;
+    .container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      height: 100vh;
+     
+      background-color:  #34495e ;
     }
     
-    th, td {
-      border: 1px solid black;
-      padding: 8px;
-      text-align: left;
+    .message {
+      font-size: 24px;
+      font-weight: bold;
+      color: white;
+      margin-bottom: 20px;
     }
     
-    th {
-      background-color: #f2f2f2;
+    .info {
+      font-size: 18px;
+      color: #bbc2ba;
     }
   </style>
 </head>
@@ -36,25 +43,21 @@
     $numero_registros = mysqli_num_rows($resultado);
     if($numero_registros != 0) {
       // Inicio de sesión exitoso
-      echo "<table>";
-      echo "<tr>";
-      echo "<th>Nombre</th>";
-      echo "<th>Edad</th>";
-      echo "<th>Cargo</th>";
-      echo "</tr>";
-      echo "<tr>";
-      echo "<td>Lisbeth de los Angeles Duarte</td>";
-      echo "<td>20</td>";
-      echo "<td>Administradora de las paginas </td>";
-      echo "</tr>";
-      echo "<tr>";
-      echo "</table>";
+      echo "<div class='container'>";
+      echo "<div class='message'>Inicio de sesión exitoso</div>";
+      echo "<div class='info'>Nombre: carlos jose reyes</div>";
+      echo "<div class='info'>Edad: 20</div>";
+      echo "<div class='info'>Cargo: soy el encargado de HelpEASA</div>";
+      echo "</div>";
     } else {
       // Credenciales inválidas
-      echo "Credenciales inválidas. Por favor, verifica tu nombre de usuario y/o contraseña."."<br>";
-      echo "Error: " . $sql . "<br>" . mysqli_error($conexion);
+      echo "<div class='container'>";
+      echo "<div class='message'>Credenciales inválidas. Por favor, verifica tu nombre de usuario y/o contraseña.</div>";
+      echo "<div class='info'>Error: " . $sql . "<br>" . mysqli_error($conexion) . "</div>";
+      echo "</div>";
     }
   }
   ?>
 </body>
 </html>
+
